c4a67243b678000dda4c84312ed36c0d05bfe7b3
HARSH <iamhp2k@gmail.com>
2020-06-22 13:54:01 +0530

    RMX1911: Add Platform Device Base Flag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,6 +42,9 @@ TARGET_USES_UEFI := true
 TARGET_BOARD_PLATFORM := trinket
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno610
 
+# Init
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
+
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image earlycon=msm_geni_serial,0x880000 loop.max_part=7 cgroup.memory=nokmem,nosocket androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
